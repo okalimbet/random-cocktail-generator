73f4224845cbf51e2312d9b6392731d9ba45264b
scripka <olga.morgan17@gmail.com>
2021-01-14 15:19:41 -0700

    Add api call and promise for a random drink

--- src/Components/App/index.js
+++ src/Components/App/index.js
@@ -1,22 +1,28 @@
+import React, { useState, useEffect } from "react";
+import { Route } from "react-router-dom";
 import './App.scss';
+import { apiCalls } from "../../apiCalls";
 
-function App() {
+const App = () => {
+  const [cocktailRandom, setCocktailRandom] = useState(null);
+  const [error, setError] = useState("");
+  const [queriedPostings, setQueriedPostings] = useState([]);
+
+  const getInfo = () => {
+    Promise.resolve(apiCalls.getRandomCocktail())
+      .then((data) => {
+        console.log(data.drinks[0])
+        setCocktailRandom(data.drinks[0])
+      })
+      .catch((err) => setError(err.message));
+  };
+
+  useEffect(() => getInfo(), {});
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="App">
+      <h1>Hello</h1>
+    </main>
   );
 }
 
---
+++ src/apiCalls.js
@@ -0,0 +1,27 @@
+const getData = (path) => {
+  return fetch(path).then((response) => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error(
+        "Sorry we are having difficulty loading this page, please try again later!"
+      );
+    }
+  });
+};
+
+const urlBody = "https://www.thecocktaildb.com/api/json/v1/1/search.php?"
+export const apiCalls = {
+
+  getRandomCocktail: () => {
+    return getData(`https://www.thecocktaildb.com/api/json/v1/1/random.php`);
+  },
+
+  getCocktailByName: (cocktailName) => {
+    return getData(`${urlBody}s=${cocktailName}`);
+  },
+
+  getCocktailByIngredient: (ingredientName) => {
+    return getData(`${urlBody}-=${ingredientName}`);
+  }
+}
