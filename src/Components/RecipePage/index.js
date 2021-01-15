a29e63652e1e3b027d7b6eb12caa96f59d70060b
scripka <olga.morgan17@gmail.com>
2021-01-15 08:50:37 -0700

    Add website structure to recipe view

--- src/Components/RecipePage/index.js
+++ src/Components/RecipePage/index.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import './RecipePage.scss';
 import { apiCalls } from "../../apiCalls";
+import { Link } from 'react-router-dom'
 
 const RecipePage = ({ randomRecipe }) => {
   const { 
@@ -8,7 +9,8 @@ const RecipePage = ({ randomRecipe }) => {
     strCategory, 
     strDrink, 
     strDrinkThumb, 
-    strGlass
+    strGlass,
+    strInstructions
   } = randomRecipe;
 
   const [ingredientsAll, setIngredientsAll] = useState([])
@@ -42,9 +44,37 @@ const RecipePage = ({ randomRecipe }) => {
   useEffect(() => getAllIngredientsMeasures(), []);
 
   return (
-    <p>this is 
-      {idDrink}
-    </p>
+    <section key={idDrink} className="recipe-view" data-testid={`recipe-view-${idDrink}`}> 
+      <h1 className="title-main">Recipe</h1>
+      <h2 className="title-drink-name">{strDrink}</h2>
+      <section className="left-side">
+        <img className="image-drink" src={strDrinkThumb}/>
+        <ul className="list-ingredients">
+          {
+            ingredientsAll.map((ingredient, index) => {
+              return (
+                <li className="ingredient">{ingredient.toLowerCase()} - {measuresAll[index].toLowerCase()}</li>
+              )
+            })
+          }
+        </ul>
+      </section>
+      <section className="right-side">
+          <h3 className="title-secondary">Instructions</h3>
+          <Link to="/favorites" className="link-redirect">
+            <h3 className="title-redirect">Go to my favorites</h3>
+            <img className="image-redirect" src=''/>
+          </Link>
+          <div className="recipe-details-container">
+            <p className="recipe-detail">{strCategory}</p>
+            <p className="recipe-detail">{strGlass}</p>
+            <p className="recipe-detail">{strInstructions}</p>
+          </div>
+          <div className="button-favorite-wrapper">
+            <img className="button-favorite"/>
+          </div>
+      </section>
+    </section>
   )
   
 }
