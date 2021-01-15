036a11ded8fa02754a9624b74a052f74187858d4
scripka <olga.morgan17@gmail.com>
2021-01-15 08:07:27 -0700

    Add sorting ingredients and measures function

--- src/Components/App/index.js
+++ src/Components/App/index.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Route } from "react-router-dom";
 import './App.scss';
 import { apiCalls } from "../../apiCalls";
+import RecipePage from "../RecipePage";
 
 const App = () => {
   const [cocktailRandom, setCocktailRandom] = useState(null);
@@ -13,16 +14,27 @@ const App = () => {
       .then((data) => {
         console.log(data.drinks[0])
         setCocktailRandom(data.drinks[0])
+        // sortingRecipeByItems(cocktailRandom)
       })
       .catch((err) => setError(err.message));
+      if(cocktailRandom) {
+      sortingRecipeByItems(cocktailRandom)
+      }
   };
 
+  const sortingRecipeByItems = (cocktail) => {
+    const cocktailData = Object.entries(cocktail)
+    const ingedients = cocktailData.reduce((ingredients, item) => {
+
+    })
+    console.log(cocktailData)
+  }
   const searchCocktailByName = (cocktailName) => {
     // const cocktailKeyword = cocktailName.toLowerCase();
     Promise.resolve(apiCalls.getCocktailByName())
       .then((data) => {
         if(data.drinks) {
-          console.log(data.drinks)
+          
         setqueriedCocktails(data.drinks);
         } else {
           console.log("sorry no such cocktail")
@@ -36,7 +48,7 @@ const App = () => {
     Promise.resolve(apiCalls.getCocktailByIngredient())
       .then((data) => {
         if(data.drinks) {
-          console.log(data)
+          // console.log(data)
         setqueriedCocktails(data.drinks);
         } else {
           console.log("sorry no such ingredients in our recipes")
@@ -46,11 +58,16 @@ const App = () => {
   }
 
   useEffect(() => getInfo(), {});
-  // useEffect(() => searchCocktailByIngredient(), []);
-
+  // useEffect(() => {sortingRecipeByItems(cocktailRandom), []);
+  
   return (
     <main className="App">
-      <h1>Hello</h1>
+      {cocktailRandom && <Route
+        exact path="/recipe/" 
+        render={()=>(
+        <RecipePage
+          randomRecipe = {cocktailRandom}/>)}/> 
+        }
     </main>
   );
 }
---
+++ src/Components/RecipePage/index.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from "react";
+import './RecipePage.scss';
+import { apiCalls } from "../../apiCalls";
+
+const RecipePage = ({ randomRecipe }) => {
+  const { 
+    idDrink, 
+    strCategory, 
+    strDrink, 
+    strDrinkThumb, 
+    strGlass
+  } = randomRecipe;
+
+  const [ingredientsItems, setIngredientsItems] = useState([])
+  const [measures, setMeasures] = useState([])
+  console.log(randomRecipe)
+  const ingredientItem = 'strIngredient';
+  const measureItem = 'strMeasure'
+  
+  const sortingRecipeByItems = (cocktail, itemName) => {
+    const cocktailData = Object.entries(cocktail)
+    const items = cocktailData
+      .reduce((items, item) => {
+        if(item[0].includes(itemName)) {
+          items.push(item[1])
+        }
+        return items;
+        },[])
+      .filter(item => item !== null);
+  
+    console.log(items)
+    return items;
+  }
+
+
+  useEffect(() => getAllIngredientsMeasures(), []);
+
+  return (
+    <p>this is 
+      {idDrink}
+    </p>
+  )
+  
+}
+
+export default RecipePage;
