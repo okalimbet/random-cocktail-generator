2febb810e1988ef448d574803830b5fd9f3b6202
scripka <olga.morgan17@gmail.com>
2021-01-16 11:13:48 -0700

    Add mock data for testing

---
+++ src/TestData/_mockData.js
@@ -0,0 +1,37 @@
+const _mockData = {
+  "drinks": [
+    {
+      "idDrink": "11202",
+      "strDrink": "Caipirinha",
+      "strDrinkAlternate": null,
+      "strTags": "ContemporaryClassic,IBA",
+      "strVideo": null,
+      "strCategory": "Ordinary Drink",
+      "strIBA": "Contemporary Classics",
+      "strAlcoholic": "Alcoholic",
+      "strGlass": "Old-fashioned glass",
+      "strInstructions": "Place lime and sugar into old fashioned glass and muddle (mash the two ingredients together using a muddler or a wooden spoon). Fill the glass with ice and add the Cachaça.",
+      "strInstructionsES": null,
+      "strInstructionsDE": "Limette und Zucker in ein old-fashioned Glas geben und vermengen (die beiden Zutaten mit einem Mixer oder einem Holzlöffel vermengen). Füllen Sie das Glas mit Eis und geben Sie den Cacha dazu.",
+      "strInstructionsFR": null,
+      "strInstructionsZH-HANS": null,
+      "strInstructionsZH-HANT": null,
+      "strDrinkThumb": "https://www.thecocktaildb.com/images/media/drink/jgvn7p1582484435.jpg",
+      "strIngredient1": "Sugar",
+      "strIngredient2": "Lime",
+      "strIngredient3": "Cachaca",
+      "strIngredient4": null,
+      "strMeasure1": "2 tsp",
+      "strMeasure2": "1 ",
+      "strMeasure3": "2 1/2 oz ",
+      "strMeasure4": null,
+      "strImageSource": "https://www.thecocktaildb.com/drink/11202-Caipirinha",
+      "strImageAttribution": "TheCocktailDB.com",
+      "strCreativeCommonsConfirmed": "Yes",
+      "dateModified": "2016-07-21 09:42:01"
+    }
+  ]
+}
+
+export default _mockData;
+
