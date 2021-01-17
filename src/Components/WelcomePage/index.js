a54998c6cee67677e790892f8d7d17a5452f9c4c
scripka <olga.morgan17@gmail.com>
2021-01-17 14:36:56 -0700

    Add basic animation and style to welcome page

--- src/Components/WelcomePage/index.js
+++ src/Components/WelcomePage/index.js
@@ -1,12 +1,13 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import './WelcomePage.scss';
 const WelcomePage = () => {
+
     return (
-      <section data-testid="welcome-page-element">
+      <section className="welcome-page-element" data-testid="welcome-page-element">
         <h1 className="welcome-page-title">Had a tough day in the office? Shake it up with a drink, you deserve this!</h1>
-        <Link to={`/recipe`} data-testid="redirect-link-recipe">
-          <button>SHAKE</button>
+        <Link to={`/recipe`} data-testid="redirect-link-recipe" className="welcome-page-link">
+          <button className="welcome-page-button">SHAKE</button>
         </Link>
       </section>
     )
